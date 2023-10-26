0107f49b7a4901fe7233ab915af18f58692719a7
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2023-10-26 21:47:09 +0530

    python programming

---
+++ Operators/operators.py
@@ -0,0 +1,38 @@
+a=10
+b=20
+
+#addition
+print(a+b)
+
+#subraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#divison
+print(a/b)
+
+#floor division
+print(a//b)
+
+#divison(&)
+print(b&a) 
+
+#membership
+x=["world"]
+print("world" in x)
+print("hello" not in x)
+
+#identity
+
+i=["penchala","vamsi"]
+j=["penchala","vamsi"]
+k=i
+print(k is i)
+print(j is k)
+print(k is not i)
+
+#exponent
+l=4
+print(l**2)
