74943732335e69aa171ab69197eea40154ba55fa
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2023-11-07 22:31:34 +0530

    Control statements

---
+++ Control Statements/control_statements.py
@@ -0,0 +1,15 @@
+a=input('Enter the value')
+b=int(a)
+if(b>18):
+    print("Major")
+else:
+    print("Minor")
+
+i=input('Enter the number')
+j=int(i)
+if((j%2)==0):
+    print("Even number")
+elif((j%2)==1):
+    print("Odd number")
+else:
+    print("Invalid number")
