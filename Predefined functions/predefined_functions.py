d415ccae43268369f765932cf407e28d09142678
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2023-11-14 21:59:11 +0530

    predefined functions

---
+++ Predefined functions/predefined_functions.py
@@ -0,0 +1,17 @@
+#range
+for i in range(1,20,2):
+    print (i)
+
+#eval
+b=eval("3+4**5/8%2-4")
+print(b)
+
+#len
+list1=[1,2,3,4]
+for c in range(len(list1)):
+    print(list1[3])
+
+list2=[2,5,6,7,9]
+print(len(list2))
+
+
